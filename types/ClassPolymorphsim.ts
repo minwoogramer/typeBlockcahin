a54e1e28e71f7aac07ebda8942f9f57dcf3f8a20
minwoo <100527953+minwoogramer@users.noreply.github.com>
2022-09-02 02:36:08 +0900

    good answer Polymorphsim

--- types/ClassPolymorphsim.ts
+++ types/ClassPolymorphsim.ts
@@ -30,116 +30,214 @@ const booleansStorage = new LocalStorage<boolean>()
 booleansStorage.get('xxx')
 booleansStorage.set('hello', true)
 
+// interface LocalStorageAPI<T> {
+//   [key: string]:T
+// }
+// class MyLocalStorage<T> {
+//     private storage : LocalStorageAPI<T> ={}
+//     setItem(key:string, value: T){
+//         this.storage[key] = value
+//     }
+//     getItem(key:string):T{
+//         return this.storage[key]
+//     }
+//     clearItem(key: string){
+//         delete this.storage[key]
+//     }
+//     clear(){
+//         this.storage = {}
+//     }
+// }
+// const LLocalStorage = new MyLocalStorage<string>();
 
+// LLocalStorage.setItem("hi", "my name is")
+// LLocalStorage.getItem("hi")
+// LLocalStorage.clearItem("hi")
+// LLocalStorage.clear()
 
-interface LocalStorageAPI<T> {
-  [key: string]:T
-}
-class MyLocalStorage<T> {
-    private storage : LocalStorageAPI<T> ={}
-    setItem(key:string, value: T){
-        this.storage[key] = value
-    }
-    getItem(key:string):T{
-        return this.storage[key]
-    }
-    clearItem(key: string){
-        delete this.storage[key]
-    }
-    clear(){
-        this.storage = {}
-    }
-}
-const LLocalStorage = new MyLocalStorage<string>();
+// interface PositionCallback{
+//     (position: Position): void
+// }
+// interface Position {
+//     coords: Coordinates
+//     timestamp: number
+// }
+// interface Coordinates {
+//     latitude: number
+//     longitude: number
+// }
+// interface PositionErrorCallback {
+//     (positionError: PositionError): void
+// }
+// interface PositionError {
+//   PERMISSION_DENIED: number
+//   POSITION_UNAVAILABLE: number
+//   TIMEOUT: number
+//   code: number
+//   message: string
+// }
+// interface Optionobj {
+//     enableHighAccuracy: boolean
+//     maximumAge: number
+//     timeout: number
+// }
+// interface GeolocationAPI {
+//  clearWatch(watchId: number): void
+//  getCurrentPosition(successCallback: PositionCallback, errorCallback?: PositionErrorCallback, options?: Optionobj): void
+//  watchPosition(successCallback: PositionCallback, errorCallback?: PositionErrorCallback, options?: Optionobj): number
+// }
 
-LLocalStorage.setItem("hi", "my name is")
-LLocalStorage.getItem("hi")
-LLocalStorage.clearItem("hi")
-LLocalStorage.clear()
+// class MyGeo implements GeolocationAPI {
+//     private  location = {}
+//     private pos:Position
+//     constructor(pos:Position) {
+//       this.pos=pos
+//     }
 
+//     clearWatch(watchId:number){
+//         delete this.location[watchId]
+//     }
+//     getCurrentPosition(successCallback: PositionCallback, errorCallback?: PositionErrorCallback, options?: Optionobj){
+//         this.location={successCallback,errorCallback,options}
+//     }
+//     watchPosition(successCallback: PositionCallback, errorCallback?: PositionErrorCallback, options?: Optionobj){
+//        return this.location={successCallback,errorCallback,options}
+//     }
+// }
 
-interface PositionCallback{
-    (position: Position): void
+// let watchId:number;
+// let target:Coordinates;
+// let optionsObj:Optionobj;
+// function successFn(pos:Position) {
+//   const crd = pos.coords;
+
+//   if (target.latitude === crd.latitude && target.longitude === crd.longitude) {
+//     console.log('Congratulations, you\'ve reached the target!');
+//     navigator.geolocation.clearWatch(watchId);
+//   }
+// };
+
+// function errorFn(err:any) {
+//   console.error(`ERROR(${err.code}): ${err.message}`);
+// };
+// watchId = 2
+// target = {
+//   latitude : 0,
+//   longitude: 0,
+// }
+
+// optionsObj = {
+//   enableHighAccuracy: false,
+//   timeout: 5000,
+//   maximumAge: 0
+// };
+
+// const geolocation = new MyGeo()
+// geolocation.getCurrentPosition(successFn);
+// geolocation.getCurrentPosition(successFn, errorFn);
+// geolocation.getCurrentPosition(successFn, errorFn, optionsObj);
+// geolocation.watchPosition(successFn);
+// geolocation.watchPosition(successFn,  errorFn);
+// geolocation.watchPosition(successFn,  errorFn, optionsObj);
+// geolocation.clearWatch(watchId);
+
+// LocalStorage Interface
+abstract class SLocalStorage<T> {
+  protected items: Items<T>
+  constructor() {
+    this.items = {}
+  }
+  abstract length(): number
+  abstract key(index: number): T
+  abstract getItem(key: string): T
+  abstract setItem(key: string, value: T): void
+  abstract removeItem(key: string): void
+  abstract clear(): void
 }
-interface Position {
-    coords: Coordinates
-    timestamp: number
+interface Items<T> {
+  [key: string]: T
 }
-interface Coordinates {
-    latitude: number
-    longitude: number
+class SuperStorage extends SLocalStorage<string> {
+  constructor() {
+    super()
+  }
+  public key(index: number) {
+    return Object.keys(this.items)[index]
+  }
+  public length() {
+    return Object.keys(this.items).length
+  }
+  public getItem(key: string) {
+    return this.items[key]
+  }
+  public setItem(key: string, value: string) {
+    this.items[key] = value
+  }
+  public removeItem(key: string) {
+    delete this.items[key]
+  }
+  public clear() {
+    this.items = {}
+  }
 }
-interface PositionErrorCallback {
-    (positionError: PositionError): void
+
+// Geolocation Interface
+type GeolocationCoords = {
+  latitude: number
+  longitude: number
+  altitude: number
+  accuracy: number
+  altitudeAccuracy: number
+  heading: number
+  speed: number
 }
-interface PositionError {
-  PERMISSION_DENIED: number
-  POSITION_UNAVAILABLE: number
-  TIMEOUT: number
+type Position = {
+  coords: GeolocationCoords
+}
+type GeoError = {
   code: number
   message: string
 }
-interface Optionobj {
-    enableHighAccuracy: boolean
-    maximumAge: number
-    timeout: number
+type SuccessFunction = (position: Position) => void
+type ErrorFunction = (error: GeoError) => void
+type GeoOptions = {
+  maximumAge: number
+  timeout: number
+  enableHighAccuracy: boolean
 }
+
+type GetCurrentPosition = {
+  (success: SuccessFunction): void
+  (success: SuccessFunction, error: ErrorFunction): void
+  (success: SuccessFunction, error: ErrorFunction, options: GeoOptions): void
+}
+
+type WatchCurrentPosition = {
+  (success: SuccessFunction): number
+  (success: SuccessFunction, error: ErrorFunction): number
+  (success: SuccessFunction, error: ErrorFunction, options: GeoOptions): number
+}
+
 interface GeolocationAPI {
- clearWatch(watchId: number): void
- getCurrentPosition(successCallback: PositionCallback, errorCallback?: PositionErrorCallback, options?: Optionobj): void
- watchPosition(successCallback: PositionCallback, errorCallback?: PositionErrorCallback, options?: Optionobj): number
+  getCurrentPosition: GetCurrentPosition
+  watchPosition: WatchCurrentPosition
+  clearWatch: (id: number) => void
 }
 
-class MyGeo implements GeolocationAPI {
-    private  location = {}
-    private pos:Position
-    constructor(pos:Position) {
-      this.pos=pos
-    }
-    
-    clearWatch(watchId:number){
-        delete this.location[watchId]
-    }
-    getCurrentPosition(successCallback: PositionCallback, errorCallback?: PositionErrorCallback, options?: Optionobj){
-        this.location={successCallback,errorCallback,options}
-    }
-    watchPosition(successCallback: PositionCallback, errorCallback?: PositionErrorCallback, options?: Optionobj){
-       return this.location={successCallback,errorCallback,options}
-    }
-}
-
-let watchId:number;
-let target:Coordinates;
-let optionsObj:Optionobj;
-function successFn(pos:Position) {
-  const crd = pos.coords;
-  
-  if (target.latitude === crd.latitude && target.longitude === crd.longitude) {
-    console.log('Congratulations, you\'ve reached the target!');
-    navigator.geolocation.clearWatch(watchId);
+class Geolocator implements GeolocationAPI {
+  getCurrentPosition: GetCurrentPosition = (
+    success: SuccessFunction,
+    error?: ErrorFunction,
+    options?: GeoOptions,
+  ) => {
+    return // Implementation goes here :)
   }
-};
-
-function errorFn(err:any) {
-  console.error(`ERROR(${err.code}): ${err.message}`);
-};
-watchId = 2
-target = {
-  latitude : 0,
-  longitude: 0,
+  watchPosition: WatchCurrentPosition = (
+    success: SuccessFunction,
+    error?: ErrorFunction,
+    options?: GeoOptions,
+  ) => {
+    return 1 // Implementation goes here :)
+  }
+  clearWatch = (id: number) => {}
 }
-
-optionsObj = {
-  enableHighAccuracy: false,
-  timeout: 5000,
-  maximumAge: 0
-};
-
-const geolocation = new MyGeo()
-geolocation.getCurrentPosition(successFn);
-geolocation.getCurrentPosition(successFn, errorFn);
-geolocation.getCurrentPosition(successFn, errorFn, optionsObj);
-geolocation.watchPosition(successFn);
-geolocation.watchPosition(successFn,  errorFn);
-geolocation.watchPosition(successFn,  errorFn, optionsObj);
-geolocation.clearWatch(watchId);
